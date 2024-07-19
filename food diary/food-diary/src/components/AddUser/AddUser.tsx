ae81e32a0a55c3290f4e305d7a1409a210328e9d
alon1303 <alon8070@gmail.com>
2024-07-19 15:07:27 +0300

    added password validation

--- food diary/food-diary/src/components/AddUser/AddUser.tsx
+++ food diary/food-diary/src/components/AddUser/AddUser.tsx
@@ -1,13 +1,36 @@
 import { useState } from "react";
 import "./AddUser.css";
+import { log } from "console";
 
 const AddUser = () => {
   const [userName, setUserName] = useState<string>("");
   const [password, setPassword] = useState<string>("");
-  function handlePassword(e:any){
 
+  function isPasswordValid(password: string) {
+    let isUpperCase: boolean = false
+    let isSpecialChar: boolean = false
+    const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+    password.split('').map((char) => {
+      if (specialChars.test(char)) {
+        isSpecialChar = true
+      }
+      if (char === char.toUpperCase()) {
+        isUpperCase = true
+      }
+    })
+    return (isUpperCase && isSpecialChar) ? true : false
   }
-  function handleSubmit() {}
+  function handlePassword(e: any) {
+    const password: string = e.target.value.toString()
+    const passwordInputClass = document.getElementById("password")
+    if (isPasswordValid(password) && passwordInputClass) {
+      setPassword(password)
+      passwordInputClass.style.border = "0.15rem solid green";
+    } else if (passwordInputClass) {
+      passwordInputClass.style.border = "0.15rem solid red";
+    }
+  }
+  function handleSubmit() { }
   return (
     <div className="add-user-container">
       <input
@@ -17,6 +40,7 @@ const AddUser = () => {
       ></input>
 
       <input
+        id="password"
         className="input"
         placeholder="Enter Password"
         onChange={handlePassword}
